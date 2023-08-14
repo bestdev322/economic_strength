2a594c2497114ce22dec95f7230427e22cdf5fb0
happylife0322 <best.dev0322@gmail.com>
2023-08-14 21:38:15 +0800

    Add some endpoints

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 var SSH2Promise = require('ssh2-promise');
 var { Client: PGClient } = require('pg');
+const request = require('request');
 const cors = require('cors');
+const fs = require("fs");
 const sshConfig = {
   username: 'crables',
   password: '!Data2020!',
@@ -14,14 +16,15 @@ const port = process.env.PORT || 3001;
 app.use(cors({
   origin: '*'
 }));
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/', (req, res) => res.send("Hello World!"));
 
 app.post('/get_esi', async (req, res) => {
   const sshClient = new SSH2Promise(sshConfig);
 
   await sshClient.connect();
 
-  const tunnel = await sshClient.addTunnel({remoteAddr: 'crables-2137.postgres.pythonanywhere-services.com', remotePort: 12137});
+  const tunnel = await sshClient.addTunnel({ remoteAddr: 'crables-2137.postgres.pythonanywhere-services.com', remotePort: 12137 });
   const pgConfig = {
     user: 'super', // or 'william' depending on the user
     password: '!Data2020!',
@@ -52,6 +55,70 @@ app.post('/get_esi', async (req, res) => {
   } catch (e) {
     res.status(400).send(e.message);
   }
+});
+
+app.get('/esi_value', (req, res) => {
+  request('https://fiscalbeat.s3.us-east-2.amazonaws.com/economic_strength/esi_value.json', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(body);
+    }
+  })
+});
+
+app.get('/esi_values', (req, res) => {
+  request('https://fiscalbeat.s3.us-east-2.amazonaws.com/economic_strength/esi_values.json', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(body);
+    }
+  })
+});
+
+app.get('/esi_recession_likelihood', (req, res) => {
+  request('https://fiscalbeat.s3.us-east-2.amazonaws.com/economic_strength/esi_recession_likelihood.json', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(body);
+    }
+  })
+});
+
+app.get('/esi_recession_likelihood_historical', (req, res) => {
+  request('https://fiscalbeat.s3.us-east-2.amazonaws.com/economic_strength/esi_recession_likelihood_historical.json', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(body);
+    }
+  })
+});
+
+app.get('/csi_data_chart_1', (req, res) => {
+  request('https://fiscalbeat.s3.us-east-2.amazonaws.com/cyclical_strength/csi_data_chart_1.json', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(body);
+    }
+  })
+});
+
+app.get('/historical_performance_chart_2', (req, res) => {
+  request('https://fiscalbeat.s3.us-east-2.amazonaws.com/cyclical_strength/historical_performance_chart_2.json', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(body);
+    }
+  })
+});
+
+app.get('/risk_adjusted_returns_chart_3', (req, res) => {
+  request('https://fiscalbeat.s3.us-east-2.amazonaws.com/cyclical_strength/risk_adjusted_returns_chart_3.json', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(body);
+    }
+  })
+});
+
+app.get('/distributions_chart_4', (req, res) => {
+  request('https://fiscalbeat.s3.us-east-2.amazonaws.com/cyclical_strength/distributions_chart_4.json', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(body);
+    }
+  })
 });
 
 app.listen(port, () => console.log(`Economic backend listening on port ${port}!`));
